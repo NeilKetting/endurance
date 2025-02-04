e64e6fedf13638bfeb9c1aa90a3ece1b6cf3ee0b
NeilKetting <40358732+NeilKetting@users.noreply.github.com>
2025-02-04 06:05:47 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,6 +20,17 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 });
 
+// Change navbar background on scroll
+window.addEventListener("scroll", function () {
+	const navbar = document.querySelector(".navbar");
+
+	if (this.window.scrollY > 0) {
+		navbar.classList.add("navbar-scroll");
+	} else {
+		navbar.classList.remove("navbar-scroll");
+	}
+});
+
 // Carousel
 let currentIndex = 0;
 
